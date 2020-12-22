3103e3556d9ff310bbd9ec6a7a3d66d51a00f699
Georgii <georgii.shestakov@7peakssoftware.com>
2020-12-22 08:58:26 +0700

    Esit init sql

--- docker-tests/init.sql
+++ docker-tests/init.sql
@@ -1,13 +1,2 @@
-USE `shoping_cart_test`;
-
-CREATE TABLE `users` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8;
-
-CREATE TABLE `cart_products` (
-    `userId` int(11) NOT NULL,
-    `productId` int(11) NOT NULL,
-    `amount` int(11) NOT NULL,
-    CONSTRAINT `cart_products_users_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
+CREATE DATABASE IF NOT EXISTS `node_backend_exesize_test`;
+USE `node_backend_exesize_test`;
