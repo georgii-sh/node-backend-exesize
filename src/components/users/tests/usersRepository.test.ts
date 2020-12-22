4140559d221771d9a59072b33c80fa7a305bde2f
Georgy Sh <kelariss@gmail.com>
2020-12-23 05:57:40 +0700

    Changed user repo to be a class

--- src/components/users/tests/usersRepository.test.ts
+++ src/components/users/tests/usersRepository.test.ts
@@ -1,8 +1,9 @@
 import Knex from 'knex'
 import { knexConfig } from 'config'
-import { createUser } from '../usersRepository'
+import UsersRepository from '../UsersRepository'
 
 const knex = Knex(knexConfig)
+const usersRepository = new UsersRepository(knex)
 
 describe('Users Repository', () => {
   beforeEach(async () => {
@@ -11,10 +12,13 @@ describe('Users Repository', () => {
     await knex.seed.run()
   })
   afterEach(async () => await knex.migrate.rollback())
-  afterAll(async () => await knex.destroy())
+  afterAll(async () => {
+    await knex.destroy()
+  })
+
   describe('createUser', () => {
     it('should return created User', async () => {
-      const created = await createUser({ name: 'Test user', email: 'test@sevenpeakssoftware.com' })
+      const created = await usersRepository.createUser({ name: 'Test user', email: 'test@sevenpeakssoftware.com' })
       expect(created).not.toBeNull()
       expect(created.email).toBe('test@sevenpeakssoftware.com')
       expect(created.name).toBe('Test user')
@@ -22,9 +26,9 @@ describe('Users Repository', () => {
     })
 
     it('should return error if email is exists', async () => {
-      expect(createUser({ name: 'Test user', email: 'john.doe@sevenpeakssoftware.com' })).rejects.toThrow(
-        /Duplicate entry/,
-      )
+      expect(
+        usersRepository.createUser({ name: 'Test user', email: 'john.doe@sevenpeakssoftware.com' }),
+      ).rejects.toThrow(/Duplicate entry/)
     })
   })
 })
--- src/components/users/usersRepository.ts
+++ src/components/users/usersRepository.ts
@@ -1,11 +1,12 @@
 import Knex from 'knex'
-import { knexConfig } from 'config'
 import User from './User'
 
-const knex = Knex(knexConfig)
+export default class UsersRepository {
+  constructor(private readonly knex: Knex) {}
 
-export async function createUser(user: { email: string; name: string }): Promise<User> {
-  const insertedId = await knex('users').insert(user).into('users')
-  const created = await knex('users').where({ userId: insertedId }).first()
-  return created
+  async createUser(user: { email: string; name: string }): Promise<User> {
+    const insertedId = await this.knex('users').insert(user).into('users')
+    const created = await this.knex('users').where({ userId: insertedId }).first()
+    return created
+  }
 }
